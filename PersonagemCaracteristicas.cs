d15e9f318f69d5397994be06d05573262e40e182
Thallison Rhanniel <thallisonrlima@gmail.com>
2017-12-06 05:25:20 -0200

    Create PersonagemCaracteristicas.cs

---
+++ PersonagemCaracteristicas.cs
@@ -0,0 +1,9 @@
+namespace ConsoleApplication
+{
+    public class PersonagemCaracteristicas : PersonagemGame
+    {
+        protected string cabeloCor { get; set; } 
+        protected string roupaCor { get; set; } 
+        protected int armaTipo { get; set; } = 1; //Valor Padrão
+    }
+}
