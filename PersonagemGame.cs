325a20e01ec9c51aa9a62a5d9277e05edbc144d9
Thallison Rhanniel <thallisonrlima@gmail.com>
2017-12-06 05:25:58 -0200

    Create PersonagemGame.cs

---
+++ PersonagemGame.cs
@@ -0,0 +1,9 @@
+namespace ConsoleApplication
+{
+    public class PersonagemGame
+    {
+        protected string nome { get; set; }
+        protected int nivel { get; set; } = 1; //Valor padrão
+        protected int poder { get; set; } = 1; //Valor padrão
+    }
+}
